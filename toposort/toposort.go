a19fb10c19343dc814bceaddb5841b52af695dbe
Tarek Khamisi <38133578+Helix-sy@users.noreply.github.com>
2025-04-08 20:39:14 +0200

    Topological sort + test 100% Coverage

--- toposort/toposort.go
+++ toposort/toposort.go
@@ -1,90 +1,60 @@
 package toposort
 
 import (
-	"errors"
+	"fmt"
 	"gitlab.lrz.de/hm/goal-core/hmgraph"
 )
 
-// TopologicalOrder computes any topological vertex order.
-// It will return an error if either the graph contains any
-// edge or if the arcs form a cycle.
-func TopologicalOrder(g *hmgraph.Graph) (sorting []*hmgraph.Vertex, err error) {
-	// Check if the graph contains any undirected edges
+func TopologicalOrder(g *hmgraph.Graph) ([]*hmgraph.Vertex, error) {
+	vertices := g.GetVertices()
+
+	//Check for undirected edges
 	hasEdge := false
-	g.ForVertices(func(v *hmgraph.Vertex) {
+	for _, v := range vertices {
 		v.ForEdges(func(e *hmgraph.Edge) {
 			hasEdge = true
 		})
-	})
+	}
 	if hasEdge {
-		return nil, errors.New("graph contains undirected edges")
+		return nil, fmt.Errorf("graph contains undirected edges")
 	}
 
-	// Count vertices for initialization
-	vertexCount := 0
-	g.ForVertices(func(v *hmgraph.Vertex) {
-		vertexCount++
-	})
+	inDegree := hmgraph.CreateVertexMap(g, "inDegree", 0)
+	defer inDegree.Dispose()
 
-	// Initialize data structures for DFS
-	visited := hmgraph.CreateVertexMap(g, "visited", false)
-	defer visited.Dispose()
+	for _, v := range vertices {
+		v.ForOutArcs(func(arc *hmgraph.Arc) {
+			target := arc.Target()
+			inDegree.Set(target, inDegree.Get(target)+1)
+		})
+	}
 
-	temp := hmgraph.CreateVertexMap(g, "temp", false)
-	defer temp.Dispose()
-
-	order := make([]*hmgraph.Vertex, 0, vertexCount)
-
-	// DFS function to detect cycles and build topological order
-	var dfs func(*hmgraph.Vertex) error
-	dfs = func(v *hmgraph.Vertex) error {
-		// If this vertex is already in the temporary mark, we have a cycle
-		if temp.Get(v) == true {
-			return errors.New("graph contains a cycle")
+	var queue []*hmgraph.Vertex
+	for _, v := range vertices {
+		if inDegree.Get(v) == 0 {
+			queue = append(queue, v)
 		}
+	}
 
-		// If we haven't visited this vertex yet
-		if visited.Get(v) == false {
-			// Mark temporarily for cycle detection
-			temp.Set(v, true)
+	var sorting []*hmgraph.Vertex
+	for len(queue) > 0 {
+		v := queue[0]
+		queue = queue[1:]
+		sorting = append(sorting, v)
 
-			// Visit all successors (outgoing arcs)
-			var cycleError error
-			v.ForOutArcs(func(arc *hmgraph.Arc) {
-				if cycleError == nil {
-					cycleError = dfs(arc.Target())
-				}
-			})
-
-			// If a cycle was detected, propagate the error
-			if cycleError != nil {
-				return cycleError
+		v.ForOutArcs(func(arc *hmgraph.Arc) {
+			target := arc.Target()
+			newIn := inDegree.Get(target) - 1
+			inDegree.Set(target, newIn)
+			if newIn == 0 {
+				queue = append(queue, target)
 			}
-
-			// Mark as permanently visited
-			visited.Set(v, true)
-			// Remove temporary mark
-			temp.Set(v, false)
-			// Add to ordering (prepend)
-			order = append([]*hmgraph.Vertex{v}, order...)
-		}
-
-		return nil
+		})
 	}
 
-	// Run DFS from each unvisited vertex
-	var finalError error
-	g.ForVertices(func(v *hmgraph.Vertex) {
-		if finalError == nil && visited.Get(v) == false {
-			if err := dfs(v); err != nil {
-				finalError = err
-			}
-		}
-	})
-
-	if finalError != nil {
-		return nil, finalError
+	if len(sorting) != len(vertices) {
+		return nil, fmt.Errorf("graph contains a cycle")
 	}
 
-	return order, nil
+	return sorting, nil
 }
